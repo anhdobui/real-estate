9611c51a48b42cca739c4f40fde2586dbe408cdf
anhdobui <anhdobui2002@gmail.com>
2024-01-15 10:18:19 +0700

    add db

--- database/insert_database.sql
+++ database/insert_database.sql
@@ -1,18 +1,323 @@
-use springbootweb;
+-- MySQL dump 10.13  Distrib 8.0.32, for Win64 (x86_64)
+--
+-- Host: localhost    Database: ex-3-p2
+-- ------------------------------------------------------
+-- Server version	8.0.32
 
-insert into role(code,name) values('ADMIN','Quản trị hệ thống');
-insert into role(code,name) values('USER','người dùng');
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-insert into users(username,password,fullname,status)
-values('admin','$2a$10$/RUbuT9KIqk6f8enaTQiLOXzhnUkiwEJRdtzdrMXXwU7dgnLKTCYG','admin',1);
-insert into users(username,password,fullname,status)
-values('nguyenvana','$2a$10$/RUbuT9KIqk6f8enaTQiLOXzhnUkiwEJRdtzdrMXXwU7dgnLKTCYG','nguyen van a',1);
+--
+-- Table structure for table `assignmentbuilding`
+--
 
-INSERT INTO user_role(user_id,role_id) VALUES (1,1);
-INSERT INTO user_role(user_id,role_id) VALUES (2,2);
+DROP TABLE IF EXISTS `assignmentbuilding`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `assignmentbuilding` (
+  `buildingid` bigint NOT NULL,
+  `staffid` bigint NOT NULL,
+  KEY `FK5ml77mpq20c7cktnaayqyndi` (`staffid`),
+  KEY `FKkk3mdegrmfcdlsxqds1m6q238` (`buildingid`),
+  CONSTRAINT `FK5ml77mpq20c7cktnaayqyndi` FOREIGN KEY (`staffid`) REFERENCES `users` (`id`),
+  CONSTRAINT `FKkk3mdegrmfcdlsxqds1m6q238` FOREIGN KEY (`buildingid`) REFERENCES `building` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `assignmentbuilding`
+--
 
+LOCK TABLES `assignmentbuilding` WRITE;
+/*!40000 ALTER TABLE `assignmentbuilding` DISABLE KEYS */;
+INSERT INTO `assignmentbuilding` VALUES (18,4),(18,5),(3,3),(3,4),(3,5),(3,6),(1,2),(1,3),(2,2),(2,3),(2,5);
+/*!40000 ALTER TABLE `assignmentbuilding` ENABLE KEYS */;
+UNLOCK TABLES;
 
+--
+-- Table structure for table `assignmentcustomer`
+--
 
+DROP TABLE IF EXISTS `assignmentcustomer`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `assignmentcustomer` (
+  `customerid` bigint NOT NULL,
+  `staffid` bigint NOT NULL,
+  KEY `FKqjgn6avtjckryyksmwrjn474o` (`staffid`),
+  KEY `FK4sygo3a6twd6tkay7em8f1lgg` (`customerid`),
+  CONSTRAINT `FK4sygo3a6twd6tkay7em8f1lgg` FOREIGN KEY (`customerid`) REFERENCES `customer` (`id`),
+  CONSTRAINT `FKqjgn6avtjckryyksmwrjn474o` FOREIGN KEY (`staffid`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `assignmentcustomer`
+--
 
+LOCK TABLES `assignmentcustomer` WRITE;
+/*!40000 ALTER TABLE `assignmentcustomer` DISABLE KEYS */;
+INSERT INTO `assignmentcustomer` VALUES (10,2),(15,2),(15,4);
+/*!40000 ALTER TABLE `assignmentcustomer` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `building`
+--
+
+DROP TABLE IF EXISTS `building`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `building` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `createdby` varchar(255) DEFAULT NULL,
+  `createddate` datetime DEFAULT NULL,
+  `modifiedby` varchar(255) DEFAULT NULL,
+  `modifieddate` datetime DEFAULT NULL,
+  `avatar` varchar(255) DEFAULT NULL,
+  `brokeragetee` decimal(13,2) DEFAULT NULL,
+  `carfee` varchar(255) DEFAULT NULL,
+  `deposit` varchar(255) DEFAULT NULL,
+  `direction` varchar(255) DEFAULT NULL,
+  `district` varchar(255) DEFAULT NULL,
+  `electricityfee` varchar(255) DEFAULT NULL,
+  `floorarea` int DEFAULT NULL,
+  `level` varchar(255) DEFAULT NULL,
+  `linkofbuilding` varchar(255) DEFAULT NULL,
+  `managername` varchar(255) DEFAULT NULL,
+  `managerphone` varchar(255) DEFAULT NULL,
+  `map` varchar(255) DEFAULT NULL,
+  `motofee` varchar(255) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `note` varchar(255) DEFAULT NULL,
+  `numberofbasement` int DEFAULT NULL,
+  `overtimefee` varchar(255) DEFAULT NULL,
+  `payment` varchar(255) DEFAULT NULL,
+  `rentprice` int DEFAULT NULL,
+  `rentpricedescription` varchar(255) DEFAULT NULL,
+  `renttime` varchar(255) DEFAULT NULL,
+  `servicefee` varchar(255) DEFAULT NULL,
+  `street` varchar(255) DEFAULT NULL,
+  `type` varchar(255) DEFAULT NULL,
+  `ward` varchar(255) DEFAULT NULL,
+  `waterfee` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=29 DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `building`
+--
+
+LOCK TABLES `building` WRITE;
+/*!40000 ALTER TABLE `building` DISABLE KEYS */;
+INSERT INTO `building` VALUES (1,NULL,NULL,'buianhdo','2023-11-16 22:16:31','/building/download.jpg',NULL,'','','','QUAN_1','',450,'','','pham van f','012456789','','','Nam Giao Building Tower','',2,'','',15,'	15 triệu/m2','','4$','59 phan xích long','NGUYEN_CAN,NOI_THAT','Phường 2',''),(2,NULL,NULL,'manager','2023-11-16 22:12:06','/building/images.jpg',NULL,'','','','QUAN_1','',650,'','','le van g','012456789','','','ACM Tower','',2,'','',18,'	18 triệu/m2','','5$','96 cao thắng','NGUYEN_CAN','Phường 4',''),(3,NULL,NULL,'buianhdo','2023-11-03 08:34:54','/building/download.jpg',NULL,'','','','QUAN_2','',200,'','','le van g','012456789','','','Alpha 2 Building Tower','',1,'','',20,'20 triệu/m2','','4$','153 nguyễn đình chiểu','NOI_THAT','Phường 6',''),(4,NULL,NULL,'buianhdo','2023-11-16 22:10:55','/building/download.jpg',NULL,'','','','QUAN_4','',200,'','','pham van f','012456789','','','IDD 1 Building','',1,'','',12,'	12 triệu/m2','','6$','111 Lý Chính Thắng','TANG_TRET,NGUYEN_CAN,NOI_THAT','Phường 7',''),(18,NULL,NULL,'buianhdo','2023-11-02 21:36:30',NULL,NULL,'','','','QUAN_2','',NULL,'','','f','01245','','','toa nha co anh','',NULL,'','',NULL,'','','','','TANG_TRET','','');
+/*!40000 ALTER TABLE `building` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `customer`
+--
+
+DROP TABLE IF EXISTS `customer`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `customer` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `createdby` varchar(255) DEFAULT NULL,
+  `createddate` datetime DEFAULT NULL,
+  `modifiedby` varchar(255) DEFAULT NULL,
+  `modifieddate` datetime DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `fullname` varchar(255) DEFAULT NULL,
+  `phone` varchar(255) DEFAULT NULL,
+  `company` varchar(255) DEFAULT NULL,
+  `deleted` bit(1) NOT NULL DEFAULT b'0',
+  `desire` varchar(255) DEFAULT NULL,
+  `note` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `customer`
+--
+
+LOCK TABLES `customer` WRITE;
+/*!40000 ALTER TABLE `customer` DISABLE KEYS */;
+INSERT INTO `customer` VALUES (10,NULL,NULL,'buianhdo','2023-11-16 17:49:40','a@gmail.com','a a a','022','',_binary '','',''),(15,NULL,NULL,'manager','2023-11-16 22:25:11','buianhdo2002@gmail.com','b b b ','0969578724','PTIT',_binary '\0','muong m','day la gi chu moi');
+/*!40000 ALTER TABLE `customer` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `rentarea`
+--
+
+DROP TABLE IF EXISTS `rentarea`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `rentarea` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `createdby` varchar(255) DEFAULT NULL,
+  `createddate` datetime DEFAULT NULL,
+  `modifiedby` varchar(255) DEFAULT NULL,
+  `modifieddate` datetime DEFAULT NULL,
+  `value` int DEFAULT NULL,
+  `buildingid` bigint DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FKqhqoxlvm1iblaew5s0v8n3ut4` (`buildingid`),
+  CONSTRAINT `FKqhqoxlvm1iblaew5s0v8n3ut4` FOREIGN KEY (`buildingid`) REFERENCES `building` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=50 DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `rentarea`
+--
+
+LOCK TABLES `rentarea` WRITE;
+/*!40000 ALTER TABLE `rentarea` DISABLE KEYS */;
+INSERT INTO `rentarea` VALUES (1,NULL,NULL,NULL,NULL,100,1),(2,NULL,NULL,NULL,NULL,200,1),(6,NULL,NULL,NULL,NULL,300,3),(7,NULL,NULL,NULL,NULL,400,3),(8,NULL,NULL,NULL,NULL,500,3),(9,NULL,NULL,NULL,NULL,100,4),(10,NULL,NULL,NULL,NULL,400,4),(11,NULL,NULL,NULL,NULL,250,4),(24,NULL,NULL,NULL,NULL,700,4),(36,'buianhdo','2023-11-03 09:10:27','buianhdo','2023-11-03 09:10:27',200,2),(37,'buianhdo','2023-11-03 09:10:27','buianhdo','2023-11-03 09:10:27',400,2),(49,'buianhdo','2023-11-03 10:35:37','buianhdo','2023-11-03 10:35:37',3,2);
+/*!40000 ALTER TABLE `rentarea` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `role`
+--
+
+DROP TABLE IF EXISTS `role`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `role` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `createdby` varchar(255) DEFAULT NULL,
+  `createddate` datetime DEFAULT NULL,
+  `modifiedby` varchar(255) DEFAULT NULL,
+  `modifieddate` datetime DEFAULT NULL,
+  `code` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UK_c36say97xydpmgigg38qv5l2p` (`code`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `role`
+--
+
+LOCK TABLES `role` WRITE;
+/*!40000 ALTER TABLE `role` DISABLE KEYS */;
+INSERT INTO `role` VALUES (1,NULL,NULL,NULL,NULL,'MANAGER','Quản trị hệ thống'),(2,NULL,NULL,NULL,NULL,'STAFF','nhân viên quản lý');
+/*!40000 ALTER TABLE `role` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `transaction`
+--
+
+DROP TABLE IF EXISTS `transaction`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `transaction` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `createdby` varchar(255) DEFAULT NULL,
+  `createddate` datetime DEFAULT NULL,
+  `modifiedby` varchar(255) DEFAULT NULL,
+  `modifieddate` datetime DEFAULT NULL,
+  `code` varchar(255) DEFAULT NULL,
+  `note` varchar(255) DEFAULT NULL,
+  `customerid` bigint DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FKldobv9jeuxje0fjqnhrw6e23v` (`customerid`),
+  CONSTRAINT `FKldobv9jeuxje0fjqnhrw6e23v` FOREIGN KEY (`customerid`) REFERENCES `customer` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `transaction`
+--
+
+LOCK TABLES `transaction` WRITE;
+/*!40000 ALTER TABLE `transaction` DISABLE KEYS */;
+INSERT INTO `transaction` VALUES (2,'anonymousUser','2023-11-16 16:13:02','anonymousUser','2023-11-16 16:13:02','QUA_TRINH_CSKH','da goi dien tu van',10),(3,'anonymousUser','2023-11-16 16:25:45','anonymousUser','2023-11-16 16:25:45','DAN_DI_AN','da dan khach hang di uong nuoc',10),(4,'buianhdo','2023-11-16 17:42:07','buianhdo','2023-11-16 17:42:07','QUA_TRINH_CSKH','a',10),(5,'buianhdo','2023-11-16 17:42:26','buianhdo','2023-11-16 17:42:26','DAN_DI_AN','đi cafe',10);
+/*!40000 ALTER TABLE `transaction` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user_role`
+--
+
+DROP TABLE IF EXISTS `user_role`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user_role` (
+  `user_id` bigint NOT NULL,
+  `role_id` bigint NOT NULL,
+  KEY `FKa68196081fvovjhkek5m97n3y` (`role_id`),
+  KEY `FKj345gk1bovqvfame88rcx7yyx` (`user_id`),
+  CONSTRAINT `FKa68196081fvovjhkek5m97n3y` FOREIGN KEY (`role_id`) REFERENCES `role` (`id`),
+  CONSTRAINT `FKj345gk1bovqvfame88rcx7yyx` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user_role`
+--
+
+LOCK TABLES `user_role` WRITE;
+/*!40000 ALTER TABLE `user_role` DISABLE KEYS */;
+INSERT INTO `user_role` VALUES (1,1),(2,2),(3,2),(4,2),(5,2),(6,2),(7,2);
+/*!40000 ALTER TABLE `user_role` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `createdby` varchar(255) DEFAULT NULL,
+  `createddate` datetime DEFAULT NULL,
+  `modifiedby` varchar(255) DEFAULT NULL,
+  `modifieddate` datetime DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `fullname` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `status` int NOT NULL,
+  `username` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UK_r43af9ap4edm43mmtq01oddj6` (`username`),
+  UNIQUE KEY `UK_6dotkott2kjsp8vw4d0m25fb7` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,NULL,NULL,NULL,NULL,NULL,'manager','$2a$10$/RUbuT9KIqk6f8enaTQiLOXzhnUkiwEJRdtzdrMXXwU7dgnLKTCYG',1,'manager'),(2,NULL,NULL,NULL,NULL,NULL,'bui anh do','$2a$10$/RUbuT9KIqk6f8enaTQiLOXzhnUkiwEJRdtzdrMXXwU7dgnLKTCYG',1,'buianhdo'),(3,NULL,NULL,NULL,NULL,NULL,'nguyen van a','$2a$10$/RUbuT9KIqk6f8enaTQiLOXzhnUkiwEJRdtzdrMXXwU7dgnLKTCYG',1,'nguyenvana'),(4,NULL,NULL,NULL,NULL,NULL,'nguyen van b','$2a$10$/RUbuT9KIqk6f8enaTQiLOXzhnUkiwEJRdtzdrMXXwU7dgnLKTCYG',1,'nguyenvanb'),(5,NULL,NULL,NULL,NULL,NULL,'phan thi a','$2a$10$/RUbuT9KIqk6f8enaTQiLOXzhnUkiwEJRdtzdrMXXwU7dgnLKTCYG',1,'phamthia'),(6,NULL,NULL,NULL,NULL,NULL,'tran thanh t','$2a$10$/RUbuT9KIqk6f8enaTQiLOXzhnUkiwEJRdtzdrMXXwU7dgnLKTCYG',1,'tranthanht'),(7,NULL,NULL,NULL,NULL,NULL,'nguyen thi v','$2a$10$/RUbuT9KIqk6f8enaTQiLOXzhnUkiwEJRdtzdrMXXwU7dgnLKTCYG',0,'nguyenthiv');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-01-15 10:16:00
